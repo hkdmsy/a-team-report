7591f195a58967f4ff363716fbc919bccc87e37d
hkdmsy <hkdmsy@gmail.com>
2026-02-22 16:47:08 +0900

    Add files via upload

---
+++ antigravity/config/firebase_config.js
@@ -0,0 +1,10 @@
+// Firebaseの設定
+const firebaseConfig = {
+    apiKey: "AIzaSyC18Bm-zHROZLreH9ClfXuHd5vB1M1l92Q",
+    authDomain: "a-team-report.firebaseapp.com",
+    databaseURL: "https://a-team-report-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "a-team-report",
+    storageBucket: "a-team-report.firebasestorage.app",
+    messagingSenderId: "473491490079",
+    appId: "1:473491490079:web:06558ab2dfea3e92415c2a"
+};
